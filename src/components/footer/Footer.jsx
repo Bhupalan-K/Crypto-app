1321d702c90f8c5a868a7f68051ae72a244a7929
Bhupalan K <bhupalan1711@gmail.com>
2024-07-20 16:45:22 -0700

    uploaded

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,8 +13,8 @@ const Footer = () => {
       <div className="icons">
         <a href="https://linkedin.com/in/bhupalan-k-487695247" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
         <a href="https://github.com/Bhupalan-K" target="_blank"><i class="fa-brands fa-github"></i></a>
-        <a href="mailto:bhupalan1711@gmail.com" target=""><i class="fa-regular fa-envelope"></i></a>
-        <a href="https://github.com/Bhupalan-K" target="_blank"><i class="fa-brands fa-whatsapp"></i></a>
+        <a href="mailto:bhupalan1711@gmail.com" target="blank"><i class="fa-regular fa-envelope"></i></a>
+        <a href="https://wa.me/7373562627" target="_blank"><i class="fa-brands fa-whatsapp"></i></a>
       </div>
       <div className='copyright'>
         <p>Copyright &copy; {year.getFullYear()}, CRYPTO - All Right Reserved.</p>
